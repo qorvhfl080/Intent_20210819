9ebd853e9715cfb71a01e8b2556f3dab379ca075
이재환 <qorvhfl080@naver.com>
2021-08-19 16:20:54 +0900

    닉네임 수정 화면에서 OK 눌렀을시 처리

--- app/src/main/java/com/neppplus/intent_20210819/EditNicknameActivity.kt
+++ app/src/main/java/com/neppplus/intent_20210819/EditNicknameActivity.kt
@@ -1,11 +1,31 @@
 package com.neppplus.intent_20210819
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nickname)
+
+        nicknameOkBtn.setOnClickListener {
+
+//            입력한 닉네임 저장
+            val inputNickname = nicknameEdt.text.toString()
+
+//            이전 화면으로 데이터를 들고 복귀
+            val resultIntent = Intent()
+            resultIntent.putExtra("nick", inputNickname)
+
+//            OK를 눌렀다고 명시 + 돌아갈 데이터 세팅
+            setResult(Activity.RESULT_OK, resultIntent)
+
+            finish()
+
+        }
+
     }
 }
