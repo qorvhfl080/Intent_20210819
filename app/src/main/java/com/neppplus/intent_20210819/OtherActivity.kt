da7342afc3c8bc1e7ee79b8e2fc71914bce32a50
이재환 <qorvhfl080@naver.com>
2021-08-19 15:33:52 +0900

    finish()

--- app/src/main/java/com/neppplus/intent_20210819/OtherActivity.kt
+++ app/src/main/java/com/neppplus/intent_20210819/OtherActivity.kt
@@ -1,11 +1,23 @@
 package com.neppplus.intent_20210819
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        moveToMainBtn.setOnClickListener {
+
+//            val returnIntent = Intent(this, MainActivity::class.java)
+//            startActivity(returnIntent)
+
+//            이전화면 복귀 : 현재 화면을 종료
+            finish()
+        }
+
     }
 }
