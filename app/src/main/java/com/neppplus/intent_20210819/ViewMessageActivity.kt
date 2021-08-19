b6f7143344aecce0a76001a14f7fb96243160fbc
이재환 <qorvhfl080@naver.com>
2021-08-19 15:42:47 +0900

    intent2 UI

---
+++ app/src/main/java/com/neppplus/intent_20210819/ViewMessageActivity.kt
@@ -0,0 +1,13 @@
+package com.neppplus.intent_20210819
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_message)
+
+
+    }
+}
