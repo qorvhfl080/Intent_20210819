ba306267397978cd78b5c7a3ef1fa3d2db2b5ec0
이재환 <qorvhfl080@naver.com>
2021-08-19 15:53:33 +0900

    전달 받은 데이터를 UI에 반영

--- app/src/main/java/com/neppplus/intent_20210819/ViewMessageActivity.kt
+++ app/src/main/java/com/neppplus/intent_20210819/ViewMessageActivity.kt
@@ -2,12 +2,18 @@ package com.neppplus.intent_20210819
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_message.*
 
 class ViewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_message)
 
+//        화면에 들어오자마자 첨부된 데이터가 있는지 확인 -> 변수에 저장
+        val receivedMessage = intent.getStringExtra("message")
+
+//        UI에 반영
+        messageTxt.text = receivedMessage
 
     }
 }
