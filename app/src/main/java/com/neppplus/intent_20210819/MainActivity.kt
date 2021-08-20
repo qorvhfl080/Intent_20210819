cd6a39a41f9354933105e411ab2a1734aecefc83
이재환 <qorvhfl080@naver.com>
2021-08-20 09:59:12 +0900

    카카오톡 플레이스토어 이동 (ACTION_VIEW)

--- app/src/main/java/com/neppplus/intent_20210819/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_20210819/MainActivity.kt
@@ -96,6 +96,16 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        kakaoStoreBtn.setOnClickListener {
+
+            val myUri = Uri.parse("market://details?id=com.kakao.talk")
+
+            val myIntent = Intent(Intent.ACTION_VIEW, myUri)
+
+            startActivity(myIntent)
+
+        }
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
